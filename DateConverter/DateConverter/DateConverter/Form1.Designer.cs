8f69d3dc519c282518ed475eb67cbb168355cff8
Milena <milenachobanova85@gmail.com>
2014-06-15 20:28:17 +0300

    Files to Date Converter

---
+++ DateConverter/DateConverter/DateConverter/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace DateConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.buttonProcess = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.inputBox = new System.Windows.Forms.Label();
+            this.outputBox = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtInput.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(50, 107);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(131, 40);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(235, 123);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(0, 10);
+            this.button.TabIndex = 2;
+            this.button.Text = "Process";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonProcess
+            // 
+            this.buttonProcess.BackColor = System.Drawing.Color.LightGray;
+            this.buttonProcess.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonProcess.Location = new System.Drawing.Point(187, 107);
+            this.buttonProcess.Name = "buttonProcess";
+            this.buttonProcess.Size = new System.Drawing.Size(77, 40);
+            this.buttonProcess.TabIndex = 3;
+            this.buttonProcess.Text = "Process";
+            this.buttonProcess.UseVisualStyleBackColor = false;
+            this.buttonProcess.Click += new System.EventHandler(this.buttonProcess_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtOutput.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(50, 167);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(131, 41);
+            this.txtOutput.TabIndex = 4;
+            this.txtOutput.TextChanged += new System.EventHandler(this.textBox2_TextChanged_1);
+            // 
+            // inputBox
+            // 
+            this.inputBox.AutoSize = true;
+            this.inputBox.BackColor = System.Drawing.Color.Silver;
+            this.inputBox.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputBox.Location = new System.Drawing.Point(50, 90);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(39, 14);
+            this.inputBox.TabIndex = 5;
+            this.inputBox.Text = "Input:";
+            this.inputBox.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // outputBox
+            // 
+            this.outputBox.AutoSize = true;
+            this.outputBox.BackColor = System.Drawing.Color.Silver;
+            this.outputBox.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputBox.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.outputBox.Location = new System.Drawing.Point(50, 150);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(47, 14);
+            this.outputBox.TabIndex = 6;
+            this.outputBox.Text = "Output:";
+            this.outputBox.Click += new System.EventHandler(this.outputBox_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Calibri", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label1.Location = new System.Drawing.Point(88, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 23);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Date Converter";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.buttonProcess;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.BackgroundImage = global::DateConverter.Properties.Resources.calendarImage1;
+            this.ClientSize = new System.Drawing.Size(297, 292);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.buttonProcess);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.txtInput);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Date Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Button buttonProcess;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label inputBox;
+        private System.Windows.Forms.Label outputBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DateConverter/DateConverter/DateConverter/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonProcess_Click(object sender, EventArgs e)
+        {
+            string[] input = txtInput.Text.Split(new char[] {',', ' ', '.'});
+            List<string> inputList = new List<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != "")
+                {
+                    inputList.Add(input[i]); 
+                }
+            }
+
+            string month = inputList[0];
+            string date = inputList[1];
+            string year = inputList[2];
+            string day = date;
+
+            if (day[0] == '0')
+            {
+                day = day.TrimStart('0'); 
+            }
+
+            switch (month)
+            {
+                case "Jan": month = "January"; break;
+                case "Feb": month = "February"; break;
+                case "Mar": month = "March"; break;
+                case "Apr": month = "April"; break;
+                case "May": month = "May"; break;
+                case "Jun": month = "June"; break;
+                case "Jul": month = "July"; break;
+                case "Aug": month = "August"; break;
+                case "Sep": month = "September"; break;
+                case "Oct": month = "October"; break;
+                case "Nov": month = "November"; break;
+                case "Dec": month = "December"; break;
+            }
+
+            string outputStr = "" + day + " " + month + " " + year; 
+            txtOutput.Text = outputStr;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void outputBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
